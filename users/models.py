8b34ff7e22845c6ba36612879a299eb8d9cb1ae4
Riccardo Fagiuoli <riccardo.fagiuoli@stud.unifi.it>
2026-05-16 18:48:34 +0200

    modifica length phone

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True, null=True, help_text="Brief biography")
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='attendee')
     date_created = models.DateTimeField(auto_now_add=True)
 
