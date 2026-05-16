c03e97a2bdb020b5c41e16cad995e47ade1b5752
Riccardo Fagiuoli <riccardo.fagiuoli@stud.unifi.it>
2026-05-16 18:50:46 +0200

    Fix max_length UserProfile field

--- users/models.py
+++ users/models.py
@@ -14,8 +14,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True, null=True, help_text="Brief biography")
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='attendee')
+    phone_number = models.CharField(max_length=50, blank=True, null=True)
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='attendee')
     date_created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
