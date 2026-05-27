903783d728d2a46ed99ec018e908070769b59f76
Riccardo Fagiuoli <riccardo.fagiuoli@stud.unifi.it>
2026-05-27 17:05:40 +0200

    Modifica CSRF_TRUSTED_ORIGINS

--- eventmenagementsystem/settings.py
+++ eventmenagementsystem/settings.py
@@ -22,7 +22,6 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 # CSRF_TRUSTED_ORIGINS - per i form
-# CSRF_TRUSTED_ORIGINS - per i form
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='https://localhost').split(',')
 # Application definition
 
