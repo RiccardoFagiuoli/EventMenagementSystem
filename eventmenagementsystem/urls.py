88234263663c25fd297a8d808466ab813b4ac7c2
Riccardo Fagiuoli <rickyfagiu33@gmail.com>
2026-06-11 01:07:43 +0200

    Modifica urls.py

--- eventmenagementsystem/urls.py
+++ eventmenagementsystem/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView, RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -21,7 +21,9 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     # Anche in produzione, servi i media (SOLO per progetto)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / 'static')
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+
 
 
