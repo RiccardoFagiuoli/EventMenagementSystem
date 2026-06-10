5c1df90809d3be48ed99e0f447d819c2f6cc0c50
Riccardo <riccardo.fagiuoli@edu.unifi.it>
2026-06-10 21:26:43 +0200

    Modifiche per allinearsi alle richieste d'esame

--- eventmenagementsystem/urls.py
+++ eventmenagementsystem/urls.py
@@ -21,6 +21,7 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     # Anche in produzione, servi i media (SOLO per progetto)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / 'static')
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
